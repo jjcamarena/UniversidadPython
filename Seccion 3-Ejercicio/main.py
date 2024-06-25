c1a48698677bd2db51cae415e5a21882cd9e6d48
Pepe <jjcamarena@gmail.com>
2024-06-24 18:28:19 -0600

    Tipos de datos en Python

--- Seccion 3-Ejercicio/main.py
+++ Seccion 3-Ejercicio/main.py
@@ -1,7 +1,9 @@
+# Declaracion de variables
 nombre = 'Juan Perez'
 telefono = 4422538521
 email = 'jperez@mail.com'
 
+# Imprimir valores
 print(nombre)
 print(telefono)
 print(email)
---
+++ Seccion 4-13/VariableTypes.py
@@ -0,0 +1,30 @@
+#
+#                                Python Data Types
+#                                        |
+#                 --------------------------------------------------
+#                |           |              |         |             |
+#            Numeric     Dictionary      Boolean     Set     Sequence Type
+#               |                                                  |
+#   --------------------------                             ----------------
+#   |             |           |                            |        |      |
+# Integer   Complex Number   Float                       Strings   List   Tuple
+
+# Tipo int
+x = 10
+print(x)
+print(type(x))
+
+# Tipo float
+x = 5.5
+print(x)
+print(type(x))
+
+# Tipo String
+x = "Hola Mundo"
+print(x)
+print(type(x))
+
+# Tipo Boolean
+x = True
+print(x)
+print(type(x))
