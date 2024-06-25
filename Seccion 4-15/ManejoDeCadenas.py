eb1be84a7c57d16bdb6fc30cfe09d12e098dd673
Pepe <jjcamarena@gmail.com>
2024-06-24 22:39:30 -0600

    Manejo de cadenas en Python

---
+++ Seccion 4-15/ManejoDeCadenas.py
@@ -0,0 +1,16 @@
+# Cadena (String)
+
+miGrupoFavorito: str = "Dream Theater"
+comentario = "La mejor banda"
+print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
+
+print("Mi grupo favorito es:", miGrupoFavorito, comentario)
+
+numero1 = "1"
+numero2 = "2"
+print("Concatenacion:", numero1 + numero2)
+print("Suma:", int(numero1) + int(numero2))
+
+numero1 = 1
+numero2 = 2
+print(numero1 + numero2)
