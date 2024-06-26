f142e4f086ecdc19998fec2db1e7e5125282de2a
Pepe <jjcamarena@gmail.com>
2024-06-25 23:16:57 -0600

    Refactorizacion del proyecto

---
+++ Seccion 5/Seccion 5-26/EjercicioRectangulo.py
@@ -0,0 +1,9 @@
+# Proporcionar los valores de alto y ancho del rectangulo
+
+alto = int(input("Proporcional el alto: "))
+ancho = int(input("Proporcional el ancho: "))
+
+area = alto * ancho
+perimetro = (alto + ancho) * 2
+
+print(f'Area: {area}, Perimetro: {perimetro}')
